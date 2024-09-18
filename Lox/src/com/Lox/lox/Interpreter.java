5b6d35a3ea48d5f5ad7726f5c3890bdd4a3df37c
Maaz Saeed <116943667+MaazSaeed@users.noreply.github.com>
2024-09-18 20:19:48 +0500

    returning from functions

--- Lox/src/com/Lox/lox/Interpreter.java
+++ Lox/src/com/Lox/lox/Interpreter.java
@@ -264,6 +264,14 @@ public class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitReturnStmt(Stmt.Return stmt) {
+        Object value = null;
+        if(stmt.value != null) value = evaluate(stmt.value);
+
+        throw new Return(value);
+    }
+
+    @Override
     public Void visitWhileStmt(Stmt.While stmt) {
         while(isTruthy(evaluate(stmt.condition))) {
             execute(stmt.body);
